053cfcdd451f2bfd3372746b4d9ded83de3d4397
DarkFreMok <153421689+DarkFreMok@users.noreply.github.com>
2023-12-18 10:53:49 +0300

    Create Kullanıcı.py

---
+++ Kullanıcı Paneli/Kullanıcı.py
@@ -0,0 +1,13 @@
+print("Oyuncu Bilgisi Bulma")
+
+a = input("Oyuncu  Adı:")
+b = input("Oyuncu Soyadı:")
+c = input("Oynadığı Takım:")
+
+bilgiler = [a,b,c]
+
+print("Bilgiler Yükleniyor...")
+
+print("Oyuncu adı: {}\n Oyuncu Soyadı: {}\n Oynadığı Takım: {}\n".format(bilgiler[0],bilgiler[1],bilgiler[2]))
+
+print("Bilgiler Yüklendi.")
